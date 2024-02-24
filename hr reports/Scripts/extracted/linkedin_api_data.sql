44a6deb03a86fab6e3f6958d38e4aee3b39e32ab
Yugin Pro <yugin.pro@gmail.com>
2024-02-24 19:51:43 +0100

    dev

---
+++ hr reports/Scripts/extracted/linkedin_api_data.sql
@@ -0,0 +1,30 @@
+--extrat threads from row mails
+
+create or replace table extracted.linkedin_20240224 ENGINE = MergeTree ORDER by id_chunk_stored as
+select category,id_chunk_stored
+,JSON_VALUE(api_chunk_stored_json, '$.request') request
+,JSON_VALUE(api_chunk_stored_json, '$.ts') request_ts
+,JSON_VALUE(api_chunk_stored_json,'$.data.metadata.keywords') metadata_keywords
+,JSON_VALUE(api_chunk_stored_json, '$.data.paging.count') paging_count
+,JSON_VALUE(api_chunk_stored_json, '$.data.paging.start') paging_start
+,JSON_VALUE(api_chunk_stored_json, '$.data.paging.total') paging_total
+,JSONExtractArrayRaw(JSON_VALUE(api_chunk_stored_json, '$.included')) included_api_chunk_stored_json
+from (
+select category, tupleElement(chunk_stored_json, 1) id_chunk_stored
+, tupleElement(chunk_stored_json, 2) api_chunk_stored_json 
+from (
+select category
+	,JSONExtractKeysAndValuesRaw(stored_json) chunk_stored_json
+from (
+select tupleElement(ess, 1) category
+, tupleElement(ess, 2) stored_json
+from(
+select JSONExtractKeysAndValuesRaw(json_result) ess
+from raw_firebase.decodeurl_51e7f_default_rtdb_linkedin_20240224
+) array
+join ess
+)
+) array
+join chunk_stored_json
+)
+settings function_json_value_return_type_allow_complex=true;
---
+++ hr reports/Scripts/extracted/linkedin_jobs.sql
@@ -0,0 +1,20 @@
+--extrat threads from row mails
+
+--create or replace table extracted.linkedin_jobs ENGINE = MergeTree ORDER by id_chunk_stored as
+--truncate table extracted.linkedin_jobs
+insert into table extracted.linkedin_jobs
+select * except(included_api_chunk_stored_json)
+, JSON_VALUE(included_api_chunk_stored_json, '$.jobPostingTitle') jobPostingTitle
+, JSON_VALUE(included_api_chunk_stored_json, '$.jobPostingUrn') jobPostingUrn
+, JSON_VALUE(included_api_chunk_stored_json, '$.trackingId') trackingId
+, JSON_VALUE(included_api_chunk_stored_json, '$.primaryDescription.text') company
+, JSON_VALUE(included_api_chunk_stored_json, '$.secondaryDescription.text') region
+, JSON_VALUE(included_api_chunk_stored_json, '$.footerItems[0].timeAt') timeAt
+, JSON_VALUE(included_api_chunk_stored_json, '$.footerItems[0].text.text') text_0
+, JSON_VALUE(included_api_chunk_stored_json, '$.footerItems[1].text.text') text_1
+, JSON_VALUE(included_api_chunk_stored_json, '$.footerItems[2].text.text') text_2
+, JSON_VALUE(included_api_chunk_stored_json, '$.logo.actionTarget') actionTarget
+, JSON_VALUE(included_api_chunk_stored_json, '$.title.text') title
+from extracted.linkedin_20240224
+array join included_api_chunk_stored_json
+settings function_json_value_return_type_allow_complex=true;
