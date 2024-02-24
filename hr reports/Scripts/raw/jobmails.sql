6a8a72a3259fdeef56be10ec496d9bb16305091b
Yugin Pro <yugin.pro@gmail.com>
2024-02-24 12:42:04 +0100

    create schema

--- hr reports/Scripts/raw/jobmails.sql
+++ hr reports/Scripts/raw/jobmails.sql
@@ -3,5 +3,5 @@ CREATE TABLE raw_firebase.decodeurl_51e7f_default_rtdb_jobmails
 
     `json_result` String
 )
-ENGINE = URL('https://decodeurl-51e7f-default-rtdb.europe-west1.firebasedatabase.app/jobmails.json',
+ENGINE = URL('https://firebasedatabase.app/jobmails.json',
  'JSONAsString');
--- hr reports/Scripts/raw/linkedin.sql
+++ hr reports/Scripts/raw/linkedin.sql
@@ -1,7 +1,7 @@
-CREATE TABLE raw_firebase.decodeurl_51e7f_default_rtdb_jobmails
+CREATE or replace TABLE raw_firebase.decodeurl_51e7f_default_rtdb_linkedin_20240224 
 (
 
     `json_result` String
 )
-ENGINE = URL('https://decodeurl-51e7f-default-rtdb.europe-west1.firebasedatabase.app/jobmails.json',
+ENGINE = URL('https://firebasedatabase.app/linkedin/voyagerJobsDashJobCards/2024/2/24.json',
  'JSONAsString');
